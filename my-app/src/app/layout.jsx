c62393fee0cd14b2d83932116d51fcfc48b0d553
Md Aftab Uddin <aftabhasan7856@gmail.com>
2026-01-21 20:14:04 +0600

    setup full project

--- my-app/src/app/layout.jsx
+++ my-app/src/app/layout.jsx
@@ -1,5 +1,11 @@
-import { Geist, Geist_Mono } from "next/font/google";
+import { Geist, Geist_Mono, Poppins } from "next/font/google";
 import "./globals.css";
+
+
+const poppins = Poppins({
+  weight:["100","200","400","600"]
+})
+
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -20,7 +26,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${poppins.className} antialiased`}
       >
         {children}
       </body>
