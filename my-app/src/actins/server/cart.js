5f019eef94a55fd42fd65665e07abd980ae15b75
Md Aftab Uddin <aftabhasan7856@gmail.com>
2026-01-23 23:51:16 +0600

    addtocart data store in mongobd

--- my-app/src/actins/server/cart.js
+++ my-app/src/actins/server/cart.js
@@ -14,9 +14,25 @@ export const handleCart = async({product,inc = true})=>{
 const query = {email:user?.email, productId:product?._id}
 const isAdded = await cartCollection.findOne(query)
 if(isAdded){
-
+    const updatedData = {
+        $inc:{
+            quantity:tnc?1:-1
+        }
+    };
+    const result  = await cartCollection.updateOne(query,updatedData)
+    return {success:Boolean(result.modifiedCount) }
 }else{
-    
+    const newData = {
+        productId:product?._id,
+        email:user?.email,
+        title:product.title,
+        quantity:1,
+        image:product.image,
+        price:product.price-(product.price * product.discount ) / 100,
+        username:user?.name,
+    }
+    const result =await cartCollection.insertOne(newData)
+    return {success:result.acknowledged}
 }
     return {success:true}
 }
